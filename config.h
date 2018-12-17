8830f2c3daf618f184b130845edb38ce54c59ac8
m2otech <m2otechonline@gmail.com>
2018-12-17 14:55:44 +0100

    Removing Alpha

--- config.h
+++ config.h
@@ -3,8 +3,8 @@
 
 #define VERSION_MAJOR 2.5
 #define VERSION_MINOR 7
-#define VERSION_IS_ALPHA 1
+#define VERSION_IS_ALPHA 0
 #define VERSION_IS_BETA 0
-#define VERSION_IS_RV 1
+#define VERSION_IS_RV 0
 
 #endif // CONFIG_H

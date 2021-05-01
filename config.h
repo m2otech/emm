c36f54e2379788d434de5b0cef5b5be12112e074
m2otech <m2otechonline@gmail.com>
2021-05-01 17:38:08 +0200

    Version 2.6.2 BETA +

--- config.h
+++ config.h
@@ -3,8 +3,8 @@
 
 #define VERSION_MAJOR 2.6
 #define VERSION_MINOR 2
-#define VERSION_IS_ALPHA 1
-#define VERSION_IS_BETA 0
+#define VERSION_IS_ALPHA 0
+#define VERSION_IS_BETA 1
 #define VERSION_IS_PLUS 1
 
 #endif // CONFIG_H
